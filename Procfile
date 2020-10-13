b96e739db4b6a8fdce52b45cde4b77f9290932a6
SoccerPlay32 <yahoosekainosekai@gmail.com>
2020-10-13 18:04:45 +0900

    'update'

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --log-file -
+web: gunicorn main:app --log-file -
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
+
 def auth():
     return os.environ.get("BEARER_TOKEN")
 
