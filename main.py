050d171c752f91f41bee967fec13e9645a72d2fb
SoccerPlay32 <yahoosekainosekai@gmail.com>
2020-10-13 20:03:04 +0900

    'frontが使いやすいデータ形式にリスポンスを変更'

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 import os
 import requests
 import json
-# from dotenv import load_dotenv
-#
-# load_dotenv()
+import datetime
+
+from dotenv import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 
@@ -13,10 +15,11 @@ def auth():
 
 def create_url(user_name):
     query ="from:"+user_name+" has:images"
+    tweet_fields = "tweet.fields=created_at"
     expansions_fields = "expansions=attachments.media_keys,author_id"
-    media_fields = "media.fields=media_key,preview_image_url,type"
-    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}".format(
-        query,expansions_fields,media_fields
+    media_fields = "media.fields=media_key,url"
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}&{}".format(
+        query,tweet_fields,expansions_fields,media_fields
     )
     return url
 
@@ -25,10 +28,32 @@ def create_headers(bearer_token):
     return headers
 
 def connect_to_endpoint(url, headers):
-    response = requests.request("GET", url, headers=headers)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    return response.json()
+    results = requests.request("GET", url, headers=headers)
+    if results.status_code != 200:
+        raise Exception(results.status_code, results.text)
+    results_json = results.json()
+    print(results_json)
+    return change_to_client_format(results_json)
+
+def change_to_client_format(search_result):
+    user_name = search_result["includes"]["users"][0]["name"]
+    media = search_result["includes"]["media"]
+
+    tweets_for_client = []
+    for tweet in search_result["data"]:
+        tweet_for_client = {
+            "author": user_name,
+            "checked": False,
+            "created_at": tweet["created_at"],
+            "text": tweet["text"],
+            "images": get_images_from_keys(tweet["attachments"]["media_keys"], media)
+        }
+        tweets_for_client.append(tweet_for_client)
+    return tweets_for_client
+
+def get_images_from_keys(media_keys, media):
+    images = [val["url"] for val in media if media_keys.count(val["media_key"])]
+    return images
 
 @app.route('/search/tweets/<string:user_name>')
 def search(user_name):
